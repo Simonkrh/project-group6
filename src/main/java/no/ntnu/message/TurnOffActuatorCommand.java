007024c80c956c8937a80cb515f98173579abe4c
Simon Horsgaard <simonkrh@gmail.com>
2023-11-23 14:57:03 +0100

    Added the TurnOffActuatorCommand

---
+++ src/main/java/no/ntnu/message/TurnOffActuatorCommand.java
@@ -0,0 +1,20 @@
+package no.ntnu.message;
+
+
+import no.ntnu.controlpanel.ControlPanelLogic;
+
+public class TurnOffActuatorCommand extends Command {
+    private final int nodeId;
+    private final int actuatorId;
+
+    public TurnOffActuatorCommand(int nodeId, int actuatorId) {
+        this.nodeId = nodeId;
+        this.actuatorId = actuatorId;
+    }
+
+    @Override
+    public Message execute(ControlPanelLogic logic) {
+        logic.onActuatorStateChanged(nodeId, actuatorId, false);
+        return new ActuatorStateMessage(nodeId, actuatorId, logic.isActuatorOn(nodeId, actuatorId));
+    }
+}
