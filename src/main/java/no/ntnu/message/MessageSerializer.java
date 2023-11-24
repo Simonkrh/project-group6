472fe3901adfe07c1eca6ce0aca3d03f377e0a0c
Simon Horsgaard <simonkrh@gmail.com>
2023-11-24 10:55:29 +0100

    Added the turnOnActuatorCommand and turnOffActuatorCommand in the MessageSerializer.

--- src/main/java/no/ntnu/message/MessageSerializer.java
+++ src/main/java/no/ntnu/message/MessageSerializer.java
@@ -11,32 +11,54 @@ public class MessageSerializer {
         if (string == null) {
             return null;
         }
+
         Message message = null;
-        switch (string) {
-            case TURN_ON_ACTUATORS_COMMAND:
-                // TODO - Implement
-                // message = new TurnOnActuatorCommand();
-                break;
-            case TURN_OFF_ACTUATORS_COMMAND:
-                // TODO - Implement
-                // message = new TurnOffActuatorCommand();
-                break;
+
+        if (string.startsWith(TURN_ON_ACTUATORS_COMMAND) || string.startsWith(TURN_OFF_ACTUATORS_COMMAND)) {
+            message = parseParametrizedMessage(string);
         }
+
         return message;
     }
 
 
     private static Message parseParametrizedMessage(String string) {
-        // TODO - implement
-        return null;
+        String[] parts = string.split(":");
+        if (parts.length < 3) {
+            return null;
+        }
+
+        String commandType = parts[0];
+        int nodeId;
+        int actuatorId;
+
+        try {
+            nodeId = Integer.parseInt(parts[1]);
+            actuatorId = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+        Message message = null;
+        switch (commandType) {
+            case TURN_ON_ACTUATORS_COMMAND:
+                message = new TurnOnActuatorCommand(nodeId, actuatorId);
+                break;
+            case TURN_OFF_ACTUATORS_COMMAND:
+                message = new TurnOffActuatorCommand(nodeId, actuatorId);
+                break;
+            default:
+                break;
+        }
+        return message;
     }
 
     public static String toString(Message message) {
         String string = null;
-        if (message instanceof TurnOnActuatorCommand) {
-            string = TURN_ON_ACTUATORS_COMMAND;
-        } else if (message instanceof TurnOffActuatorCommand) {
-            string = TURN_OFF_ACTUATORS_COMMAND;
+        if (message instanceof TurnOnActuatorCommand turnOnActuatorCommand) {
+            string = TURN_ON_ACTUATORS_COMMAND + ":" + turnOnActuatorCommand.getNodeId() + ":" + turnOnActuatorCommand.getActuatorId();
+        } else if (message instanceof TurnOffActuatorCommand turnOffActuatorCommand) {
+            string = TURN_OFF_ACTUATORS_COMMAND + ":" + turnOffActuatorCommand.getNodeId() + ":" + turnOffActuatorCommand.getActuatorId();
         }
         return string;
     }
