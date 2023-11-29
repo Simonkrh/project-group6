ad531b1f907e651c9a0e71a23db875b01c66a07c
Simon Horsgaard <simonkrh@gmail.com>
2023-11-29 11:44:49 +0100

    Added some missing javadoc, and implemented error message in the MessageSerializer.

--- src/main/java/no/ntnu/message/CommunicationChannelCommand.java
+++ src/main/java/no/ntnu/message/CommunicationChannelCommand.java
@@ -3,6 +3,10 @@ package no.ntnu.message;
 import no.ntnu.controlpanel.CommunicationChannel;
 import no.ntnu.controlpanel.ControlPanelLogic;
 
+/**
+ * Represents a command for retrieving the current state of the
+ * communication channel in the control panel logic.
+ */
 public class CommunicationChannelCommand extends Command {
     @Override
     public Message execute(ControlPanelLogic logic) {
