1d0b29d5c4b69182f2b9160734e4f468800a01bf
Andreas Malmedal <andreas07.malmedal@gmail.com>
2023-12-07 21:50:46 +0100

    Removed javadoc for override method.

--- src/main/java/no/ntnu/controlpanel/SocketCommunicationChannel.java
+++ src/main/java/no/ntnu/controlpanel/SocketCommunicationChannel.java
@@ -47,12 +47,6 @@ public class SocketCommunicationChannel implements CommunicationChannel {
 
     }
 
-    /**
-     * Sends the acturare state  over  the channel.
-     * @param nodeId     ID of the node to which the actuator is attached
-     * @param actuatorId Node-wide unique ID of the actuator
-     * @param isOn       When true, actuator must be turned on; off when false.
-     */
     @Override
     public void sendActuatorChange(int nodeId, int actuatorId, boolean isOn) {
         if (socket != null && socket.isConnected()) {
