23c71b04db29d097a24798a6570a68aa2a01c2c9
mohamed3873 <mohamed36912@hotmail.com>
2023-12-11 09:03:36 +0100

    added javadoc to the noderemovemessage class

--- src/main/java/no/ntnu/message/NodeRemovedMessage.java
+++ src/main/java/no/ntnu/message/NodeRemovedMessage.java
@@ -1,12 +1,31 @@
 package no.ntnu.message;
 
+/**
+ * class represents a message indicating the removal of a node.
+ * It implements the Message interface.
+ *
+ */
 public class NodeRemovedMessage implements Message {
+
+    /**
+     * The ID of the node that has been removed.
+     */
     private int nodeId;
 
+    /**
+     * Constructs a new noderemovemessage with the specified node ID.
+     *
+     * @param nodeId The ID of the node that has been removed.
+     */
     public NodeRemovedMessage(int nodeId) {
         this.nodeId = nodeId;
     }
 
+    /**
+     * Gets the ID of the removed node.
+     *
+     * @return The ID of the removed node.
+     */
     public int getNodeId() {
         return nodeId;
     }
